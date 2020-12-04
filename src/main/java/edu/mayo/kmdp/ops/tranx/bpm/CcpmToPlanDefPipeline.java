a70b25f97a9f6e5ba445a50484f99c7635dada2f
Davide Sottara <Sottara.Davide@mayo.edu>
2020-12-04 12:54:52 -0600

    [#270546] Map DMN Decision Services to FHIR PlanDefinition

--- src/main/java/edu/mayo/kmdp/ops/tranx/bpm/CcpmToPlanDefPipeline.java
+++ src/main/java/edu/mayo/kmdp/ops/tranx/bpm/CcpmToPlanDefPipeline.java
@@ -14,8 +14,10 @@
 package edu.mayo.kmdp.ops.tranx.bpm;
 
 import static java.util.Arrays.asList;
+import static org.omg.spec.api4kp._20200801.AbstractCarrier.ofHeterogeneousComposite;
 import static org.omg.spec.api4kp._20200801.AbstractCarrier.rep;
 import static org.omg.spec.api4kp._20200801.services.transrepresentation.ModelMIMECoder.encode;
+import static org.omg.spec.api4kp._20200801.taxonomy.krlanguage.KnowledgeRepresentationLanguageSeries.CMMN_1_1;
 import static org.omg.spec.api4kp._20200801.taxonomy.krlanguage.KnowledgeRepresentationLanguageSeries.FHIR_STU3;
 import static org.omg.spec.api4kp._20200801.taxonomy.lexicon.LexiconSeries.PCV;
 import static org.omg.spec.api4kp._20200801.taxonomy.lexicon.LexiconSeries.SNOMED_CT;
@@ -28,7 +30,6 @@ import edu.mayo.kmdp.knowledgebase.constructors.DependencyBasedConstructor;
 import edu.mayo.kmdp.knowledgebase.flatteners.dmn.v1_2.DMN12ModelFlattener;
 import edu.mayo.kmdp.knowledgebase.flatteners.fhir.stu3.PlanDefinitionFlattener;
 import edu.mayo.kmdp.knowledgebase.selectors.fhir.stu3.PlanDefSelector;
-import edu.mayo.kmdp.knowledgebase.weavers.fhir.stu3.DMNDefToPlanDefWeaver;
 import edu.mayo.kmdp.language.LanguageDeSerializer;
 import edu.mayo.kmdp.language.TransrepresentationExecutor;
 import edu.mayo.kmdp.language.parsers.cmmn.v1_1.CMMN11Parser;
@@ -37,9 +38,11 @@ import edu.mayo.kmdp.language.translators.cmmn.v1_1.CmmnToPlanDefTranslator;
 import edu.mayo.kmdp.language.translators.dmn.v1_2.DmnToPlanDefTranslator;
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import org.omg.spec.api4kp._20200801.Answer;
 import org.omg.spec.api4kp._20200801.api.inference.v4.server.ReasoningApiInternal._askQuery;
@@ -66,8 +69,6 @@ public class CcpmToPlanDefPipeline implements _applyNamedTransform {
   KnowledgeAssetRepositoryApi repo;
   TermsApi terms;
 
-  ResourceIdentifier dictionaryAssetId;
-  ResourceIdentifier dictionaryArtifactId;
   URI[] annotationVocabularies;
 
   DeserializeApiInternal parser;
@@ -78,6 +79,8 @@ public class CcpmToPlanDefPipeline implements _applyNamedTransform {
 
   _flattenArtifact flattener;
 
+  _flattenArtifact dmnFlattener;
+
   _assembleCompositeArtifact assembler;
 
   KnowledgeBaseProvider kbManager;
@@ -85,20 +88,15 @@ public class CcpmToPlanDefPipeline implements _applyNamedTransform {
   _askQuery dataShapeQuery;
 
 
-
   protected Map<Integer, Consumer<Answer<KnowledgeCarrier>>> injectors = new HashMap<>();
 
   public CcpmToPlanDefPipeline(
-      ResourceIdentifier dictionaryAssetId,
-      ResourceIdentifier dictionaryArtifactId,
       @Autowired KnowledgeAssetCatalogApi cat,
       @Autowired KnowledgeAssetRepositoryApi repo,
       @Autowired TermsApi terms,
       _askQuery dataShapeQuery,
       URI... annotationVocabularies
   ) {
-    this.dictionaryAssetId = dictionaryAssetId;
-    this.dictionaryArtifactId = dictionaryArtifactId;
     this.cat = cat;
     this.repo = repo;
     this.terms = terms;
@@ -123,26 +121,27 @@ public class CcpmToPlanDefPipeline implements _applyNamedTransform {
     flattener
         = new PlanDefinitionFlattener();
 
+    dmnFlattener
+        = new DMN12ModelFlattener();
+
     assembler
         = GraphBasedAssembler.newInstance(repo);
 
     kbManager
         = new KnowledgeBaseProvider(repo)
-        .withNamedWeaver(
-            kbp -> new DMNDefToPlanDefWeaver(kbp, terms, dictionaryArtifactId.getResourceId()))
         .withNamedSelector(kbp -> new PlanDefSelector(kbp))
-        .withNamedBinder(kbp -> new PlanDefDataShapeBinder(kbp))
-        .withNamedFlattener(kbp -> new DMN12ModelFlattener(kbp));
+        .withNamedBinder(kbp -> new PlanDefDataShapeBinder(kbp));
 
   }
 
   public CcpmToPlanDefPipeline addInjector(int index, Consumer<Answer<KnowledgeCarrier>> consumer) {
-    injectors.put(index,consumer);
+    injectors.put(index, consumer);
     return this;
   }
 
   public Consumer<Answer<KnowledgeCarrier>> injector(int j) {
-    return injectors.getOrDefault(j, kc -> {});
+    return injectors.getOrDefault(j, kc -> {
+    });
   }
 
 
@@ -170,34 +169,17 @@ public class CcpmToPlanDefPipeline implements _applyNamedTransform {
         composite.flatMap(kc -> parser.applyLift(kc, Abstract_Knowledge_Expression));
     injector(1).accept(parsedComposite);
 
-    // Weave the dictionary in
-    // TODO Definitely need to smoothen out the APIs
-    KnowledgeCarrier dictionary
-        = repo.getCanonicalKnowledgeAssetCarrier(dictionaryAssetId.getUuid(),dictionaryAssetId.getVersionTag())
-        .flatMap(kc -> parser.applyLift(kc,Abstract_Knowledge_Expression))
-        .orElseThrow(IllegalStateException::new);
-
-    Answer<KnowledgeCarrier> wovenComposite =
-        parsedComposite.flatMap(kc ->
-            // TODO: 03/12/2020 where we want to get to with id updates
-            kbManager.initKnowledgeBase(kc)
-                .flatMap(pid ->
-                    kbManager.weave(pid.getUuid(), pid.getVersionTag(), dictionary))
-                .flatMap(wid ->
-                    kbManager.flatten(wid.getUuid(),wid.getVersionTag(),null))
-                .flatMap(dId ->
-                    kbManager.getKnowledgeBaseManifestation(dId.getUuid(), dId.getVersionTag())));
+    Answer<KnowledgeCarrier> wovenComposite = flattenDecisions(parsedComposite);
 
     injector(2).accept(wovenComposite);
 
-  // Translate into PlanDefinition
+    // Translate into PlanDefinition
     Answer<KnowledgeCarrier> planDefinitions =
         wovenComposite.flatMap(kc ->
             translator.applyTransrepresent(kc, encode(rep(FHIR_STU3, SNOMED_CT, PCV)), null))
             // TODO The translator should maybe preserve the AssetID ?
             .map(kc -> kc.withAssetId(struct.get().getAssetId()));
     injector(3).accept(planDefinitions);
-
 
     // Flatten the composite, which at this point is homogeneous FHIR PlanDef
     Answer<KnowledgeCarrier> planDefinition = planDefinitions
@@ -217,13 +199,15 @@ public class CcpmToPlanDefPipeline implements _applyNamedTransform {
             pdPtr.getUuid(), pdPtr.getVersionTag(),
             PlanDefSelector.id, PlanDefSelector.pivotQuery(annotationVocabularies), null))
         .flatMap(conceptsPtr ->
-                kbManager
-                    .getKnowledgeBaseManifestation(conceptsPtr.getUuid(), conceptsPtr.getVersionTag())
-                    .flatMap(selectedConcepts ->
-                        dataShapeQuery.askQuery(null, null, selectedConcepts))
-                    .map(bindingList -> bindingList.get(0))
-        .flatMap(bindings -> planDefKB.flatMap(pd -> kbManager.bind(pd.getUuid(),pd.getVersionTag(),bindings)))
-        .flatMap(ptr -> kbManager.getKnowledgeBaseManifestation(ptr.getUuid(),ptr.getVersionTag())));
+            kbManager
+                .getKnowledgeBaseManifestation(conceptsPtr.getUuid(), conceptsPtr.getVersionTag())
+                .flatMap(selectedConcepts ->
+                    dataShapeQuery.askQuery(null, null, selectedConcepts))
+                .map(bindingList -> bindingList.get(0))
+                .flatMap(bindings -> planDefKB
+                    .flatMap(pd -> kbManager.bind(pd.getUuid(), pd.getVersionTag(), bindings)))
+                .flatMap(ptr -> kbManager
+                    .getKnowledgeBaseManifestation(ptr.getUuid(), ptr.getVersionTag())));
 
     injector(5).accept(shapedPlanDef);
 
@@ -231,5 +215,35 @@ public class CcpmToPlanDefPipeline implements _applyNamedTransform {
     return shapedPlanDef;
   }
 
+  private Answer<KnowledgeCarrier> flattenDecisions(Answer<KnowledgeCarrier> parsedComposite) {
+    // TODO - see if/how to improve this whole method
+    CompositeKnowledgeCarrier ckc = (CompositeKnowledgeCarrier) parsedComposite.get();
+    List<KnowledgeCarrier> allComps = ckc.componentList();
+
+    List<KnowledgeCarrier> flatComps = allComps.stream()
+        .map(kc -> {
+          if (kc.getRepresentation().getLanguage().sameAs(CMMN_1_1)) {
+            return kc;
+          } else {
+            ResourceIdentifier rootId = kc.getAssetId();
+            return dmnFlattener
+                .flattenArtifact(
+                    ofHeterogeneousComposite(allComps).withRootId(rootId),
+                    rootId.getUuid())
+                .orElseThrow(() -> new RuntimeException());
+          }
+        }).collect(Collectors.toList());
+
+    ckc.components()
+        .forEach(comp -> {
+          KnowledgeCarrier flatComp = flatComps.stream()
+              .filter(f -> comp.getAssetId().asKey().equals(f.getAssetId().asKey()))
+//              .filter(f -> allComps.indexOf(comp) == flatComps.indexOf(f))
+              .findFirst()
+              .orElseThrow();
+          comp.setExpression(flatComp.getExpression());
+        });
+    return Answer.of(ckc);
+  }
 
 }
