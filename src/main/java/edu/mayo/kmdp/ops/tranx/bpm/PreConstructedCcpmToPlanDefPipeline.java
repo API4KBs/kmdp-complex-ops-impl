bc3009dd732a04a909f542d743207edb26fff132
Davide Sottara <Sottara.Davide@mayo.edu>
2021-10-07 22:22:52 -0500

    [#1024180] Combine cCPM and PlanDef jobs

--- src/main/java/edu/mayo/kmdp/ops/tranx/bpm/PreConstructedCcpmToPlanDefPipeline.java
+++ src/main/java/edu/mayo/kmdp/ops/tranx/bpm/PreConstructedCcpmToPlanDefPipeline.java
@@ -5,9 +5,7 @@ import org.omg.spec.api4kp._20200801.Answer;
 import org.omg.spec.api4kp._20200801.api.inference.v4.server.ReasoningApiInternal._askQuery;
 import org.omg.spec.api4kp._20200801.api.repository.asset.v4.KnowledgeAssetCatalogApi;
 import org.omg.spec.api4kp._20200801.api.repository.asset.v4.KnowledgeAssetRepositoryApi;
-import org.omg.spec.api4kp._20200801.api.terminology.v4.TermsApi;
 import org.omg.spec.api4kp._20200801.id.Pointer;
-import org.omg.spec.api4kp._20200801.id.ResourceIdentifier;
 import org.omg.spec.api4kp._20200801.services.KnowledgeCarrier;
 
 public class PreConstructedCcpmToPlanDefPipeline extends CcpmToPlanDefPipeline {
@@ -18,14 +16,11 @@ public class PreConstructedCcpmToPlanDefPipeline extends CcpmToPlanDefPipeline {
       _askQuery dataShapeQuery,
       URI... annotationVocabularies) {
     super(cat, repo, dataShapeQuery, annotationVocabularies);
-
   }
 
   @Override
   public Answer<Pointer> initKnowledgeBase(KnowledgeCarrier kc, String params) {
-    ResourceIdentifier compositeId = kc.getAssetId();
-    return repo.getCompositeKnowledgeAssetCarrier(compositeId.getUuid(), compositeId.getVersionTag())
-        .flatWhole(m -> kbManager.initKnowledgeBase(m, params));
+    return kbManager.initKnowledgeBase(kc, params);
   }
 
 }
